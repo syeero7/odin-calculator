577dee13702ea4f9b3d52c73b47b448bff6fca38
SyeerO7 <EmalHasitha@proton.me>
2024-08-20 18:17:01 +0530

    Add a function to display the values

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
-const display = document.querySelector("#display")
-const buttons = document.querySelector("ul")
+const display = document.querySelector("#display");
+const buttons = document.querySelector("ul");
 
-let firstOperand =""
-let secondOperand = ""
-let currentOperator = null
+let firstOperand = "";
+let secondOperand = "";
+let currentOperator = null;
 
 const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
 
-const operate = (operator,num1,num2) => {};
+const operate = (operator, num1, num2) => {};
 
-buttons.addEventListener("click",(event)=>{
-    const target = event.target
+buttons.addEventListener("click", (event) => {
+  const target = event.target;
 
-    switch (target.className) {
-        case "number":
-            
-            break;
-        case "operator":
+  switch (target.className) {
+    case "number":
+      displayValues(target.textContent);
 
-            
-            break;
-        case "equals":
+      break;
+    case "operator":
+      chooseOperation(target.textContent);
 
-            
-            break;
-        case "clear":
+      break;
+    case "equals":
+      break;
+    case "clear":
+      break;
+  }
+});
 
-            
-            break;    
-    }
-})
+function displayValues(value) {
+  if (display.textContent === "") return;
+  display.textContent += value;
+}
+
+function chooseOperation(operator) {
+  firstOperand = display.textContent;
+  currentOperator = operator;
+}
