ed0ef3393eb30c98e8cbb956060e4be7a36f21e6
SyeerO7 <EmalHasitha@proton.me>
2024-08-20 18:05:09 +0530

    Add an event listener to get button click events

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
+const display = document.querySelector("#display")
+const buttons = document.querySelector("ul")
+
+let firstOperand =""
+let secondOperand = ""
+let currentOperator = null
+
 const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
+
+const operate = (operator,num1,num2) => {};
+
+buttons.addEventListener("click",(event)=>{
+    const target = event.target
+
+    switch (target.className) {
+        case "number":
+            
+            break;
+        case "operator":
+
+            
+            break;
+        case "equals":
+
+            
+            break;
+        case "clear":
+
+            
+            break;    
+    }
+})
